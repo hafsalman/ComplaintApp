1158080f013ac2c4c2ce9b2a31f96e1f6c921c27
Hafsa Salman <hafsalman0521@gmail.com>
2026-06-07 22:00:35 +0500

    Login

---
+++ api/login.php
@@ -0,0 +1,17 @@
+<?php
+include "db.php";
+
+$data = json_decode(file_get_contents("php://input"));
+
+$username = $data->username;
+$password = $data->password;
+
+$sql="SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result=$conn->query($sql);
+
+if($result->num_rows>0){
+    echo json_encode(["success"=>true]);
+}else{
+    echo json_encode(["success"=>false]);
+}
+?>
